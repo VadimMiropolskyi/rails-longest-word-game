957fe986534433f65239f644645bddd9a5235c9d
VadimMiropolskyi <wadim.mir0602@gmail.com>
2023-02-20 16:39:09 +0100

    add def score logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
     def new
         alphabet = ('A'..'Z').to_a
@@ -5,6 +8,20 @@ class GamesController < ApplicationController
     end
 
     def score
-        raise
+        word = params[:word]
+        letters = params[:letters].split(' ')
+
+        if word.upcase.chars.all? { |char| letters.include?(char) }
+            url = "https://wagon-dictionary.herokuapp.com/#{word}"
+            response = URI.open(url).read
+            result = JSON.parse(response)
+            if result['found']
+                @score = "Congratulations! #{word} is a valid English word!"
+            else
+                @score = "#{word} is not a valid English word."
+            end
+        else
+            @score = "#{word} can't be built out of the original grid."
+        end
     end
 end
